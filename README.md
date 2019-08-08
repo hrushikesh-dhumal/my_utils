2381a23bf70dde5b2e5e078ac10f1fffea385ab2
hrushikesh-dhumal <hrushikesh.dhumal@gmail.com>
2019-08-08 23:16:00 +0530

    added create_jupyter_env

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # my_utils
 Simple utility functions
+
+
+## Manage anaconda environments
+```bash
+conda create -n my_env python=3.7
+# Activate environement
+conda activate my_env
+# if using windows: conda.bat activate my_env
+# Add environment to ipython kernel
+pip install ipykernel
+python -m ipykernel install --name my_env
+# Deactivate the environment
+conda deactivate
+# if using windows: conda.bat deactivate
+# Delete environment
+conda remove --name my_env --all
+jupyter kernelspec uninstall my_env
+```
---
+++ create_jupyter_env.sh
@@ -0,0 +1,46 @@
+# Sample usage source create_jupyter_env.sh hru python3.7
+ENV_NAME=$1
+PYTHON_VERSION=$2
+
+if [ -z "$ENV_NAME" ]
+then
+      printf "ENV_NAME is empty. Correct usage:\n\tsource create_jupyter_env.sh <YOUR_ENVIRONMENT_NAME> <PYTHON_VERSION>"
+      return 2
+fi
+
+if [ -z "$PYTHON_VERSION" ]
+then
+      printf "PYTHON_VERSION is empty, Setting to default python3.7"
+      PYTHON_VERSION="python3.7"
+fi
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        OS="linux"
+elif [[ "$OSTYPE" == "darwin" ]]; then
+        OS="mac"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+        OS="windows"
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+        OS="windows"
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        OS="linux"
+# else
+        # Unknown.
+fi
+
+echo "Creating " $ENV_NAME
+conda create -n $ENV_NAME python=$PYTHON_VERSION
+if [[ "$OS" == "windows" ]]; then
+        conda.bat activate $ENV_NAME
+else
+        conda activate $ENV_NAME
+fi
+pip install ipykernel
+python -m ipykernel install --name $ENV_NAME
+if [[ "$OS" == "windows" ]]; then
+        conda.bat deactivate
+else
+        conda deactivate
+fi
